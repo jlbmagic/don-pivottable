78f070d32c2c683c97a1aa789c44d7d056a1de75
Jeremy Brown <jlbmagic@gmail.com>
2022-03-12 08:12:55 -0500

    set file to pull from FM

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ const jquery = require("jquery");
 window.$ = window.jQuery = jquery; // notice the definition of global variables here
 require("jquery-ui-dist/jquery-ui.js");
 import "pivottable";
-// import "pivottable/dist/c3_renderers.js";
-// import "pivottable/dist/export_renderers";
-import { data } from "./data";
+import "pivottable/dist/c3_renderers.js";
+import "pivottable/dist/export_renderers";
 
-console.log(data);
+window.loadPivotTable = (data) => {
+  const obj = JSON.parse(data);
 
-$("#output").pivotUI(data, {
-  rows: ["City", "State"],
-  cols: ["Branches"],
-  vals: ["Amt All Del Loans"],
-  aggregatorName: "Sum",
-  rendererName: "Table",
-});
+  $.pivotUtilities.data = obj;
+  $("#output").pivotUI(obj, {
+    rows: ["City", "State"],
+    cols: ["Branches"],
+    vals: ["Amt All Del Loans"],
+    aggregatorName: "Sum",
+    rendererName: "Table",
+    renderers: $.extend(
+      $.pivotUtilities.renderers,
+      $.pivotUtilities.c3_renderers,
+      $.pivotUtilities.export_renderers,
+      $.pivotUtilities.subtotal_renderers
+    ),
+  });
+};
